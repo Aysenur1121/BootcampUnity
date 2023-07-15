1b47681fa498054c0b157d6733c04444ae2a63d7
Aysenur1121 <nurero1121@gmail.com>
2023-07-15 16:57:14 +0300

    SingleEnd scene temel

---
+++ BioBuddies/Assets/Scripts/GoMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoMenu : MonoBehaviour
+{
+    public void GoMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
